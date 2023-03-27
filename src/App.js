1d4d7f658714ce297bcca56206ba1b6559591cc7
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-27 19:04:54 +0530

    Basic scaffolding for ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="App">
+      <h2>React 17</h2>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
