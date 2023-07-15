835ced70136745aad93f7f978b6cb7fa70a73b4d
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-15 16:13:12 +0530

    feat: Add app.ts file with server setup and routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!!");
+import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+import routes from "./routes";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.listen(port, async () => {
+  logger.info("Express server listening on port " + port);
+  await connect();
+
+  routes(app);
+});
