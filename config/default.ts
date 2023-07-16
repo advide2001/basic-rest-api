3c17c601fa0dfc7cfb20ab85d1da01415d7f2c92
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-16 15:08:26 +0530

    feat: Add saltWorkFactor to default config

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,5 @@ export default {
   port: 1337,
   dbUri:
     "mongodb+srv://desaiadarsh22:1234@cluster0.7ubymtl.mongodb.net/basic-rest-api-db?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
 };
