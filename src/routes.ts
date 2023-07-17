5e98ef351aace4cd168372310f2495c3b6b7216c
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-17 12:31:43 +0530

    feat: Add user and session routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validate from "./middleware/validateResource";
+import { createUserSchema } from "./schema/user.schema";
+import { createUserSessionHandler } from "./controller/session.controller";
+import { createSessionSchema } from "./schema/session.schema";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => {
     res.sendStatus(200);
   });
+
+  app.post("/api/users", validate(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/sessions",
+    validate(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 
 export default routes;
