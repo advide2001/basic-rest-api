d3019a5595a3f55d6753311ef713e79bc16c89dd
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-17 12:29:57 +0530

    feat: Add body parser middleware for JSON parsing

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ const port = config.get<number>("port");
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, async () => {
   logger.info("Express server listening on port " + port);
   await connect();
