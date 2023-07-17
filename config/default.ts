eca2b6185aefab58ced5424a5c10af411cbcad55
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-17 11:23:52 +0530

    chore: Remove config file from repository

--- config/default.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  port: 1337,
-  dbUri:
-    "mongodb+srv://desaiadarsh22:1234@cluster0.7ubymtl.mongodb.net/basic-rest-api-db?retryWrites=true&w=majority",
-  saltWorkFactor: 10,
-};
